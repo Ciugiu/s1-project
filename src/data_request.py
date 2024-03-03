1f45d5c4e528f66f521a746b1e864f4434acde3a
Ciugiu <153821557+Ciugiu@users.noreply.github.com>
2024-03-03 01:16:01 +0100

    Update App

--- src/data_request.py
+++ src/data_request.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mysql.connector
 
 def connect():
@@ -21,28 +20,4 @@ def retrieve(query: str):
 #with open('src/active_population', 'r') as file:
 #    raw_student_population = retrieve(file.read())
 #
-=======
-import mysql.connector
-
-def connect():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="123456",
-        database="project"
-    )
-
-def retrieve(query: str):
-    mysql = connect()
-    cursor = mysql.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    cursor.close()
-    mysql.close()
-    return result
-
-#with open('src/active_population', 'r') as file:
-#    raw_student_population = retrieve(file.read())
-#
->>>>>>> e8b59a0471c883f3840e95294ddf0e06a44744d4
 #print(raw_student_population)
